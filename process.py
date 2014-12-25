f732d5d928fbabb9fd83da20104aee24bdad4a7b
nkfly <nkflyfly@gmail.com>
2014-12-25 17:49:05 +0800

    lala

--- process.py
+++ process.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 				dimension_list.append(dimension)
 			else:
 				for i in range(1,len(entries)):
-					if (column[i-1] == '區域別總計'):
+					if column[i-1] == '區域別總計' or column[i-1]=='消防署' or column[i-1] == '基隆港' or column[i-1] == '臺中港' or column[i-1] == '高雄港' or column[i-1] == '花蓮港' :
 						continue
 					if (entries[0]+column[i-1]) not in reorder_dict:
 						reorder_dict[(entries[0]+column[i-1])] = {dimension : entries[i]}
@@ -23,6 +23,7 @@ if __name__ == "__main__":
 			w.write('時間地點,')
 			for d in dimension_list:
 				w.write(d + ',')
+			w.write('消防車/土地面積(平方公里),消防車/人口數(人),救災車/土地面積(平方公里),救災車/人口數(人),消防勤務車/土地面積(平方公里),消防勤務車/人口數(人),救護車輛/土地面積(平方公里),救護車輛/人口數(人),消防人力總計/土地面積(平方公里),消防人力總計/人口數(人)')
 			w.write('\n')
 
 			for k in reorder_dict:
@@ -32,6 +33,16 @@ if __name__ == "__main__":
 						w.write(reorder_dict[k][d] + ',')
 					else :
 						w.write( '0,')
+				w.write(str(float(reorder_dict[k]['消防車'])/float(reorder_dict[k]['土地面積(平方公里)']))+',')
+				w.write(str(float(reorder_dict[k]['消防車'])/float(reorder_dict[k]['人口數(人)']))+',')
+				w.write(str(float(reorder_dict[k]['救災車'])/float(reorder_dict[k]['土地面積(平方公里)']))+',')
+				w.write(str(float(reorder_dict[k]['救災車'])/float(reorder_dict[k]['人口數(人)']))+',')
+				w.write(str(float(reorder_dict[k]['消防勤務車'])/float(reorder_dict[k]['土地面積(平方公里)']))+',')
+				w.write(str(float(reorder_dict[k]['消防勤務車'])/float(reorder_dict[k]['人口數(人)']))+',')
+				w.write(str(float(reorder_dict[k]['救護車輛'])/float(reorder_dict[k]['土地面積(平方公里)']))+',')
+				w.write(str(float(reorder_dict[k]['救護車輛'])/float(reorder_dict[k]['人口數(人)']))+',')
+				w.write(str(float(reorder_dict[k]['消防人力總計'])/float(reorder_dict[k]['土地面積(平方公里)']))+',')
+				w.write(str(float(reorder_dict[k]['消防人力總計'])/float(reorder_dict[k]['人口數(人)']))+',')
 				w.write('\n')
 		w.close()
 
